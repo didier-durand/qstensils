ad03d7810ab2d837a1c31cde37a5fa5c1119a393
Didier Durand <didduran@amazon.ch>
2024-02-22 14:31:13 +0100

    q_list_docs v1

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
-# qstensils
-Ustensils / tools for Amazon Q
+# qstensils: utility scripts for Amazon Q
+
+Diverse utility scripts to explore and operate Amazon Q for Business Use. 
+
+Amazon Q is a fully managed, generative-AI powered assistant that you can configure to answer questions, provide summaries, generate content, and complete tasks based on data in your enterprise. Amazon Q provides immediate and relevant information to employees, and helps streamline tasks and accelerate problem solving.
+
+## To obtain a list indexed documents with their metadata
+to obtain a full list of all documents, enter on command line from /src directory:
+```shell
+python3 list_docs.py --app_id <q-app-id> --idx_id <q_idx-id>  
+```
+
+some example:
+``` 
+    % python3 q_list_docs.py --app_id 64ce5747-3e5-4ec-a43-10c14d159f3 --idx_id 6b850c2-3e9-440-b4c-3dcabd8015 --json --inv
+
+    <.....> 
+    {
+        "createdAt": "2024-02-21 11:31:00.618000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Ying xiong.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:25.755000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.500000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Watership Down.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:30.810000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.236000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Unforgiven.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:02.884000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.238000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Viskningar och rop.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:14.965000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.422000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Togo.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:09.220000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.709000+01:00",
+        "documentId": "s3://aws-q-didier-durand/What Ever Happened to Baby Jane?.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:46.031000+01:00"
+    },
+    {
+        "createdAt": "2024-02-21 11:31:00.698000+01:00",
+        "documentId": "s3://aws-q-didier-durand/Vicky Donor.json",
+        "error": {},
+        "status": "INDEXED",
+        "updatedAt": "2024-02-21 11:47:53.677000+01:00"
+    }
+]
+--- document inventory
+INDEXED: 992
+DOCUMENT_FAILED_TO_INDEX: 7
+TOTAL: 999
+```
+
+For help, use -h or --help option:
+```
+% python3 q_list_docs.py -h                                                                                                  
+usage: q_list_docs.py [-h] [-app APP_ID] [-idx IDX_ID] [-j] [-incl INCLUDE] [-excl EXCLUDE] [-inv] [-v]
+
+list documents indexed by Amazon Q
+
+options:
+  -h, --help            show this help message and exit
+  -app APP_ID, --app_id APP_ID
+                        Q application id
+  -idx IDX_ID, --idx_id IDX_ID
+                        Q index id
+  -j, --json            json format for results
+  -incl INCLUDE, --include INCLUDE
+                        comma-separated list of status to include
+  -excl EXCLUDE, --exclude EXCLUDE
+                        comma-separated list of status to exclude
+  -inv, --inventory     with document inventory
+  -v, --verbose         verbose mode
+```
+
+## Security
+
+The scripts assume that the user reflected by environment variables named `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` has proper credentials to access the APIs of Amazon Q. See [IAM policy examples](https://docs.aws.amazon.com/amazonq/latest/business-use-dg/security_iam_id-based-policy-examples.html) in the [Security section](https://docs.aws.amazon.com/amazonq/latest/business-use-dg/security-iam.html) of Q Documentation for all details.
+
---
+++ src/q_list_docs.py
@@ -0,0 +1,103 @@
+import json
+from pprint import pprint
+
+import boto3
+import argparse
+
+q_client = boto3.client("qbusiness") # noqa
+
+
+def check_response(response: dict, http_status_code=200) -> bool:
+    assert response['ResponseMetadata']['HTTPStatusCode'] == http_status_code
+    return True
+
+
+def list_documents(app_id="", idx_id="", verbose=False) -> list[dict]:
+    r_docs: list[dict] = list[dict]()
+    next_token: str = ""
+    while True:
+        if next_token == "":
+            # maxResults cannot be bigger than 100 currently
+            response = q_client.list_documents(applicationId=app_id, indexId=idx_id,
+                                               maxResults=100)
+            if verbose:
+                pprint(response)
+            check_response(response)
+        else:
+            response = q_client.list_documents(applicationId=app_id, indexId=idx_id,
+                                               maxResults=100, nextToken=next_token)
+            if verbose:
+                pprint(response)
+            check_response(response)
+        if "documentDetailList" in response:
+            docs: list = response["documentDetailList"]
+            if len(docs) > 0:
+                r_docs.extend(docs)
+        if "nextToken" not in response or response["nextToken"] == "":
+            break
+        else:
+            next_token = response["nextToken"]
+    if len(r_docs) > 0:
+        return r_docs
+
+
+def pretty_print(docs: list[dict] = None):
+    for i, doc in enumerate(docs):
+        print("#" + str(i) + ":")
+        for key in doc.keys():
+            print("   " + key + ":", str(doc[key]))
+
+
+def inventory(docs):
+    states: dict[str, int] = dict[str, int]()
+    for doc in docs:
+        status = doc['status']
+        if status not in states.keys():
+            states[status] = 0
+        states[status] += 1
+    states["TOTAL"] = len(docs)
+    print("--- document inventory")
+    for status in states:
+        print(status + ":", states[status])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="list documents indexed by Amazon Q")
+    parser.add_argument("-app", "--app_id", type=str, help="Q application id")
+    parser.add_argument("-idx", "--idx_id", type=str, help="Q index id")
+    parser.add_argument("-j", "--json", action="store_true", help="json format for results")
+    parser.add_argument("-incl", "--include", type=str, help="comma-separated list of status to include")
+    parser.add_argument("-excl", "--exclude", type=str, help="comma-separated list of status to exclude")
+    parser.add_argument("-inv", "--inventory", action="store_true", help="with document inventory")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
+    args = parser.parse_args()
+
+    if args.app_id is None or args.idx_id is None:
+        raise ValueError("Q application id and index id must be specified - usage: python3 q_list_docs.py "
+                         "--app_id <q-app-id> --idx_id <q-idx-id>")
+    if args.include is not None and args.exclude is not None:
+        raise ValueError("either --include or --exclude list of status can be specified but not both simultaneously")
+    if args.verbose:
+        print("Q API list_docs() called with app_id:", args.app_id, " - idx-id:", args.idx_id)
+
+    documents: list[dict] = list_documents(app_id=args.app_id, idx_id=args.idx_id, verbose=args.verbose)
+    filtered: list[dict] = list[dict]()
+    if args.include:
+        status_list = args.include.split(",")
+        for document in documents:
+            if document["status"] in status_list:
+                filtered.append(document)
+    elif args.exclude:
+        status_list = args.exclude.split(",")
+        for document in documents:
+            if document["status"] not in status_list:
+                filtered.append(document)
+    else:
+        filtered = documents
+    if args.json:
+        print(json.dumps(filtered, indent=4, default=str))
+    else:
+        pretty_print(filtered)
+
+    if args.inventory:
+        inventory(documents)
