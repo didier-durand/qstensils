6c33e45ed68905572ff4ed2c3cec4cff9fc2c459
Didier Durand <didduran@amazon.ch>
2024-02-26 07:04:16 +0100

    refactoring

--- src/q_list_conversations.py
+++ src/q_list_conversations.py
@@ -12,8 +12,8 @@ def check_response(response: dict, http_status_code=200) -> bool:
     return True
 
 
-def list_conversations(app_id="", usr_id="", verbose=False) -> list[dict] | None:
-    r_conversations: list[dict] = list[dict]()
+def list_conversations(app_id: str = "", usr_id: str = "", verbose: bool = False) -> list[dict] | None:
+    convs: list[dict] = list[dict]()
     next_token: str = ""
     while True:
         if next_token == "":
@@ -32,18 +32,17 @@ def list_conversations(app_id="", usr_id="", verbose=False) -> list[dict] | None
         if "conversations" in response:
             convs: list = response["conversations"]
             if len(convs) > 0:
-                r_conversations.extend(convs)
-        if "nextToken" not in response or response["nextToken"] == "":  # pylint: disable=no-else-break
+                convs.extend(convs)
+        if "nextToken" not in response or response["nextToken"] == "":
             break
-        else:
-            next_token = response["nextToken"]
-    if len(r_conversations) > 0:
-        return r_conversations
+        next_token = response["nextToken"]
+    if len(convs) > 0:
+        return convs
     return None
 
 
-def list_messages(app_id="", usr_id="", conv_id="", verbose=False) -> list[dict] | None:
-    r_messages: list[dict] = list[dict]()
+def list_messages(app_id: str = "", usr_id: str = "", conv_id: str = "", verbose: bool = False) -> list[dict] | None:
+    msgs: list[dict] = list[dict]()
     next_token: str = ""
     while True:
         if next_token == "":
@@ -60,36 +59,33 @@ def list_messages(app_id="", usr_id="", conv_id="", verbose=False) -> list[dict]
                 pprint(response)
             check_response(response)
         if "messages" in response:
-            convs: list = response["messages"]
-            if len(convs) > 0:
-                r_messages.extend(convs)
-        if "nextToken" not in response or response["nextToken"] == "":  # pylint: disable=no-else-break
+            r_msgs: list = response["messages"]
+            if len(r_msgs) > 0:
+                msgs.extend(r_msgs)
+        if "nextToken" not in response or response["nextToken"] == "":
             break
-        else:
-            next_token = response["nextToken"]
-    if len(r_messages) > 0:
-        return r_messages
+        next_token = response["nextToken"]
+    if len(msgs) > 0:
+        return msgs
     return None
 
 
-def pretty_print(convs: list[dict] = None):
-    for i, conv in enumerate(convs):
-        print("#conv-" + str(i) + ":")
-        for key in conv.keys():
-            if key == "messages":
-                for j, msg in enumerate(conv[key]):
-                    print("   #msg-" + str(j) + ":")
-                    for key2 in msg:
-                        print("      " + key2 + ":", str(msg[key2]))
-            else:
-                print("   " + key + ":", str(conv[key]))
+def list_conversations_with_messages(app_id: str = "", usr_id: str = "", verbose=False) -> list[dict] | None:
+    convs: list[dict] = list_conversations(app_id=app_id, usr_id=usr_id, verbose=verbose)
+    if convs is not None and len(convs) > 0:
+        for conv in convs:
+            messages: list[dict] = list_messages(app_id=app_id, usr_id=usr_id,
+                                                 conv_id=conv["conversationId"], verbose=verbose)
+            if messages is not None and len(messages) > 0:
+                conv['messages'] = messages
+        return convs
+    return None
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="list documents indexed by Amazon Q")
     parser.add_argument("-a", "--app_id", type=str, help="Q application id")
     parser.add_argument("-u", "--usr_id", type=str, help="Q user id")
-    parser.add_argument("-j", "--json", action="store_true", help="json format for results")
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
     args = parser.parse_args()
 
@@ -97,16 +93,8 @@ if __name__ == "__main__":
         raise ValueError("Q application id and user id must be specified - usage: python3 q_list_documents.py "
                          "--app_id <q-app-id> --usr_id <q-usr-id>")
     if args.verbose:
-        print("Q API list_docs() called with app_id:", args.app_id, " - usr-id:", args.usr_id)
+        print("Q API list_conversations() called with app_id:", args.app_id, " - usr-id:", args.usr_id)
 
-    conversations: list[dict] = list_conversations(app_id=args.app_id, usr_id=args.usr_id, verbose=args.verbose)
-    for conversation in conversations:
-        messages: list[dict] = list_messages(app_id=args.app_id, usr_id=args.usr_id,
-                                             conv_id=conversation["conversationId"], verbose=args.verbose)
-        if messages is not None and len(messages) > 0:
-            conversation['messages'] = messages
-
-    if args.json:
-        print(json.dumps(conversations, indent=4, default=str))
-    else:
-        pretty_print(conversations)
+    conversations: list[dict] = list_conversations_with_messages(app_id=args.app_id, usr_id=args.usr_id,
+                                                                 verbose=args.verbose)
+    print(json.dumps(conversations, indent=4, default=str))
